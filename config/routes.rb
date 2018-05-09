efd9eb8e46993778bf2a680979fa047f4cc2c30b
kyuuki <fdear.kobayashi@nifty.com>
2018-05-09 09:41:30 +0000

    ユーザー登録、ログイン画面をきれいに

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :applications
-  resources :accounts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "ipo_companies#index"
 
   devise_for :users
+
   resources :ipo_companies
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :accounts
+  resources :applications
 end
